151ce508a0854f213e615c07d92e941ef3a57eec
rakib12212 <rakibulislamr83@gmail.com>
2025-03-13 00:14:01 +0600

    testimonial update

--- src/data/TestimonialData.js
+++ src/data/TestimonialData.js
@@ -11,14 +11,14 @@ export const TestimonialData = [
   {
     name: "AFRID BIN NAFSI",
     role: "Digital Marketing Specialist",
-    image: "assets.testimonial.nafsi",
+    image: assets.testimonial.nafsi,
     quote:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     name: "ABIDUR RAHMAN",
     role: "Virtual Assistant",
-    image: "assets.testimonial.abir",
+    image: assets.testimonial.abir,
     quote:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
