ba265aa466c7e7215a38ece71777d4ba96c928d6
rakib12212 <rakibulislamr83@gmail.com>
2025-03-12 17:47:02 +0600

    add location

--- src/component/UI/homepage/pages/Location.jsx
+++ src/component/UI/homepage/pages/Location.jsx
@@ -8,16 +8,14 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 23.803411200583852, // Your Latitude
-  lng:  90.37631130020584, // Your Longitude
+  lat: 23.80182, // Your Latitude
+  lng: 90.37631, // Your Longitude
 };
 
 const Location = () => {
   return (
     <div>
-      <LoadScript
-        googleMapsApiKey={process.env.MapsApiKey}
-      >
+      <LoadScript googleMapsApiKey={process.env.MapsApiKey}>
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
           <Marker position={center} />
         </GoogleMap>
