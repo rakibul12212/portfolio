207c7daea64b049dc363ccdea8002adacff9dde9
rakib12212 <rakibulislamr83@gmail.com>
2025-03-22 03:28:07 +0600

    add custom sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import Navbar from "@/component/shared/Navbar";
 import "./globals.css";
 import Footer from "@/component/shared/Footer";
 
-
 export const metadata = {
   title: "Rakibul Islam Portfolio",
   description: "Rakibul Islam Portfolio",
@@ -10,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="custom-scroll overflow-y-scroll h-screen">
       <body className="bg-[#000000] ">
         <Navbar />
         {children}
