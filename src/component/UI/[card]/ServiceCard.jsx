964ab725f1c7f0d28338ffeb49539f43c9de3014
rakib12212 <rakibulislamr83@gmail.com>
2025-03-22 02:37:13 +0600

    service animation

--- src/component/UI/[card]/ServiceCard.jsx
+++ src/component/UI/[card]/ServiceCard.jsx
@@ -2,6 +2,19 @@
 import { ServiceData } from "@/data/ServiceData";
 import { getServiceIcon } from "@/utils/getServiceIcon";
 import React from "react";
+import { motion } from "framer-motion";
+
+const cardVariants = {
+  hidden: { opacity: 0, y: 60 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
 
 const ServiceCard = () => {
   return (
@@ -9,9 +22,13 @@ const ServiceCard = () => {
       {ServiceData.map(({ number, icon, title, description }) => {
         const Icon = getServiceIcon(icon);
         return (
-          <div
+          <motion.div
             key={number}
             className="border-b-1 border-purple-700 rounded-md bg-gradient-to-r from-purple-300 to-blue-900"
+            variants={cardVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: false, amount: 0.2 }} // animate on scroll
           >
             <div className="bg-neutral-800 shadow-md rounded-md p-5 flex flex-col md:flex-row md:items-center md:justify-between gap-4 hover:bg-gradient-to-r from-purple-500/40 to-blue-500/40 transition-all duration-300">
               {/* Number */}
@@ -20,7 +37,6 @@ const ServiceCard = () => {
               </p>
 
               {/* Title + Icon */}
-
               <div className="flex items-center gap-2">
                 {Icon && <Icon className="text-2xl md:text-3xl text-white" />}
                 <span className="text-xl md:text-2xl font-semibold text-white">
@@ -33,7 +49,7 @@ const ServiceCard = () => {
                 {description}
               </p>
             </div>
-          </div>
+          </motion.div>
         );
       })}
     </div>
