6f59e688498d9ae127ba86985494817627b82905
rakib12212 <rakibulislamr83@gmail.com>
2025-03-23 04:06:01 +0600

    loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import Navbar from "@/component/shared/Navbar";
 import "./globals.css";
+import Navbar from "@/component/shared/Navbar";
 import Footer from "@/component/shared/Footer";
-
 
 export const metadata = {
   title: "Rakibul Islam Portfolio",
@@ -10,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className="bg-[#000000] ">
         <Navbar />
         {children}
